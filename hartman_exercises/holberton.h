c0fe7b08c9fa4916ca634e62f381e5aa1af2ca73
jacobbhartman <209@holbertonschool.com>
2017-11-12 21:29:30 +0000

    initial commit, have yet to comple a super simple shell

---
+++ hartman_exercises/holberton.h
@@ -0,0 +1,12 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/* libraries and directives */
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#endif /* _HOLBERTON_H_ */
